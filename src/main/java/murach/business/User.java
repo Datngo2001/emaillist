8866e34950355a17c46511f50fba77f73579a467
Dat Ngo <ngominhdat110115@gmail.com>
2021-11-01 15:42:53 +0700

    Chap13ex01

--- src/main/java/murach/business/User.java
+++ src/main/java/murach/business/User.java
@@ -1,25 +1,37 @@
 package murach.business;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable {
 
+    private Long userId;
+    private String email;
     private String firstName;
     private String lastName;
-    private String email;
 
-    public User() {
-        firstName = "";
-        lastName = "";
-        email = "";
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getUserId() {
+        return userId;
     }
 
-    public User(String firstName, String lastName, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
     }
-
+    
     public String getFirstName() {
         return firstName;
     }
@@ -34,13 +46,5 @@ public class User implements Serializable {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
--- src/main/java/murach/email/EmailListServlet.java
+++ src/main/java/murach/email/EmailListServlet.java
@@ -1,68 +1,57 @@
 package murach.email;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 import murach.business.User;
 import murach.data.UserDB;
-import murach.data.UserIO;
 
-public class EmailListServlet extends HttpServlet
-{    
+public class EmailListServlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/index.html";
 
         // get current action
         String action = request.getParameter("action");
         if (action == null) {
-            action = "join";  // default action
+            action = "join"; // default action
         }
 
         // perform action and set URL to appropriate page
-        String url = "/index.jsp";
         if (action.equals("join")) {
-            url = "/index.jsp";    // the "join" page
-        }
-        else if (action.equals("add")) {                
+            url = "/index.jsp"; // the "join" page
+        } else if (action.equals("add")) {
             // get parameters from the request
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
             String email = request.getParameter("email");
 
-            // store data in User object and save User object in database
-            User user = new User(firstName, lastName, email);
-            UserDB.insert(user);
-            
-            // set User object in request object and set URL
+            // store data in User object
+            User user = new User();
+            user.setEmail(email);
+            user.setFirstName(firstName);
+            user.setLastName(lastName);
+
+            // validate the parameters
+            String message;
+            if (UserDB.emailExists(user.getEmail())) {
+                message = "This email address already exists.<br>" + "Please enter another email address.";
+                url = "/index.jsp";
+            } else {
+                message = "";
+                url = "/thanks.jsp";
+                UserDB.insert(user);
+            }
             request.setAttribute("user", user);
-            url = "/thanks.jsp";   // the "thanks" page
+            request.setAttribute("message", message);
         }
-        
-        // create the Date object and store it in the request
-        Date currentDate = new Date();
-        request.setAttribute("currentDate", currentDate);
+        getServletContext().getRequestDispatcher(url).forward(request, response);
+    }
 
-        // create users list and store it in the session
-        String path = getServletContext().getRealPath("/WEB-INF/EmailList.txt");
-        ArrayList<User> users = UserIO.getUsers(path);
-        HttpSession session = request.getSession();
-        session.setAttribute("users", users);
-
-        // forward request and response objects to specified URL
-        getServletContext()
-            .getRequestDispatcher(url)
-            .forward(request, response);
-    }    
-    
-    @Override
-    protected void doGet(HttpServletRequest request, 
-                          HttpServletResponse response) 
-                          throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
-    }    
+    }
 }
